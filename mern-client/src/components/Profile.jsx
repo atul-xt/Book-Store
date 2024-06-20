c75710cee0b55be4757e02b077053def09b01162
atul-xt <atul.xtt@gmail.com>
2024-06-20 14:26:14 +0530

    profile photo bug fixed

--- mern-client/src/components/Profile.jsx
+++ mern-client/src/components/Profile.jsx
@@ -5,14 +5,12 @@ import userImg from "../assets/profile.jpg";
 
 const Profile = () => {
   const { user } = useContext(AuthContext);
-  console.log(user);
   return (
     <div className="px-4 my-4">
       <h2 className="mb-8 text-3xl font-bold">Profile</h2>
 
       <div className="flex lg:w-[900px] flex-col flex-wrap gap-4 bg-gray-300 rounded-2xl p-20">
-        <img className="h-32 w-32 rounded-lg " src={user?.photoURL} alt="" />
-        <h2 className="font-semibold">UID: {user?.uid || "UID"}</h2>
+        <img className="h-32 w-32 rounded-lg " src={user?.photoURL || userImg} alt="" />
         <h2 className="font-semibold">Name: {user?.displayName || "Demo"}</h2>
         <h2 className="font-semibold">Email: {user?.email || "demo@gmail.com"}</h2>
         <h2 className="font-semibold">Verified User: {user?.emailVerified ? "Yes" : "No" || "Not Verified yet"}</h2>
